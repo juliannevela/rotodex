7d22f7010b1c6959607d91fafabbddfce9d31387
Juli Vela <75726146+juliannevela@users.noreply.github.com>
2024-10-03 18:03:50 -0700

    added/config nativewind/tailwindcss

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
-import { Text, View } from "react-native";
+import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl">Rotodex</Text>
+      <Link href="/profile">Go to profile</Link>
     </View>
   );
 }
